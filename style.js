8a381bed1f17002d63d2788291a71bcee5ae2f74
Mkashif768 <145900041+Mkashif768@users.noreply.github.com>
2024-02-17 11:00:12 +0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,63 @@
+
+// 1. Sum of all digits of a number.
+
+
+// let numbers = [1, 2, 3, 4, 5];
+// let sum = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+
+// console.log(sum);
+
+
+//2. Sum of the range of 2 numbers. 
+
+// let start = 1;
+// let end = 10;
+// let sum = 0;
+
+// for (let i = start; i <= end; i++) {
+//   sum += i;
+// }
+
+// console.log(sum);
+
+// 3. Calculate average.
+
+// let numbers = [1, 2, 3, 4, 5];
+// let sum = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+
+// let average = sum / numbers.length;
+
+// console.log(average);
+
+
+
+let str1 = "Hello";
+let str2 = "World";
+
+if (str1.length === str2.length) {
+  console.log("The strings have the same number of characters.");
+} else if (str1.length > str2.length) {
+  console.log("The first string has more characters than the second string.");
+} else {
+  console.log("The second string has more characters than the first string.");
+}
+
+
+
+// 5. Swap the string's first and last character. 
+
+// let str = "Hello";
+// let firstChar = str[0];
+// let lastChar = str[str.length - 1];
+// let swappedStr = lastChar + str.slice(1, -1) + firstChar;
+
+// console.log(swappedStr);
+
